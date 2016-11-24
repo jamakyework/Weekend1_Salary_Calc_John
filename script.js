6c2af2605b4361d296daa096775b7c07859abdd4
John Amakye <john.amakye@minneapolismn.gov>
2016-11-24 15:14:11 -0600

    intial commit

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ console.log("it works");
 var userInput = [];
 console.log(userInput);
 
-// var userInfo = document.getElementById("firstName");
-// console.log("user entered: ", userInfo);
+
 
 function clickFunction() {
   console.log(document.getElementById("firstName").value);
@@ -13,38 +12,3 @@ function clickFunction() {
   console.log(document.getElementById("jobTitleIn").value);
   console.log(document.getElementById("annualSalIn").value);
 }
-
-
-// var addItem = function(fname, lname, idNum, jobTitle, annualSal){
-//   console.log('in addItem: ', fname, lname, idNum, jobTitle, annualSal);
-//   var newItem = {
-//   fname: fname,
-//   lname: lname,
-//   idNum: idNum,
-//   jobTitle: jobTitle,
-//   annualSal: annualSal,
-// };
-// userInput.push( newItem );
-// };
-
-//   //get user input
-// function getUserInput(){
-//   var firstName =
-//   ( 'first name:', fnameIn);
-//   var lastName = document.getElementById('lnameIn').value;
-// console.log( 'last name:', lnameIn );
-//   var idNum = document.getElementById('idNumIn').value;
-//  console.log( 'Id Numb:', idNumIn);
-//   // send user input to addItem
-//   addItem( fnameIn, lnameIn, idNumIn );
-// }// end getUserInput
-
-
-// function myFunction() {
-//     var getInput = document.getElementById("fname").value ;
-//                    document.getElementById("lname").value ;
-//                    document.getElementById("idNum").value ;
-//                    document.getElementById("jobTitle").value ;
-//                    document.getElementById("annualSal").value ;
-//  console.log(getInput);
-//   }
