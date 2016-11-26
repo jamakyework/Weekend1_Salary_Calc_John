4ae3cb3300b248fa94e33c4f908bd67a61d3c466
John Amakye <john.amakye@minneapolismn.gov>
2016-11-26 09:42:55 -0600

    step 1 completed, input form created

--- readme.md
+++ readme.md
@@ -1,6 +1,20 @@
-[]The application should first have an input form that collects the following:
-Employee First Name
-Employee Last Name
-ID Number
-Job Title
-Annual Salary
+Weekend Project ReadMe
+=======================
+
+[x]  The application should first have an input form that collects the following:
+    Employee First Name,
+    Employee Last Name,
+    ID Number,
+    Job Title,
+    Annual Salary,
+
+[/]  A 'Submit' button should clear out the inputs and your logic should store
+    that information.
+
+[]  Then, that information should be appended to the DOM so
+    the user of the application can see the information they just entered.
+
+[]  Finally, your logic should calculate all of the employee salaries and report
+    back the Monthly cost of  salaries.
+    (use loop to calculate salaries, can use an alert or console log to report
+    monthly cost of salaries)
--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 console.log("it works");
 
+// userInput array
 var userInput = [];
-console.log(userInput);
+//addItem
+var addItem = function( name1, name2, idNum1, jobTitle1, annualSal1 ){
+//end addItem
+console.log( 'this is in addItem1:', name1, name2, idNum1, jobTitle1, annualSal1);
+// create newitem object
+  var newItem = {
+  name1:      name1,
+  name2:      name2,
+  idNum1:     idNum1,
+  jobTitle1:  jobTitle1,
+  annualSal1: annualSal1
+};
+   // end newItem object
+   // push newItem into the userInput array
+     userInput.push(newItem);
+   // end userInput push
+};
+console.log("This is in userInput: ", userInput);
 
 
-
-function clickFunction() {
-  console.log(document.getElementById("firstName").value);
-  console.log(document.getElementById("lastName").value);
-  console.log(document.getElementById("idNumIn").value);
-  console.log(document.getElementById("jobTitleIn").value);
-  console.log(document.getElementById("annualSalIn").value);
-}
+// start clickFunction(acquire user input)
+ var clickFunction = function() {
+  var firstName = document.getElementById("firstNameIn").value;
+                  //console.log(document.getElementById("firstNameIn").value);
+  var lastName =  document.getElementById("lastNameIn").value;
+                  //console.log(document.getElementById("lastNameIn").value);
+  var idNum =     document.getElementById("idNumIn").value;
+                  //console.log(document.getElementById("idNumIn").value);
+  var jobTitle =  document.getElementById("jobTitleIn").value;
+                  //console.log(document.getElementById("jobTitleIn").value);
+  var annualSal = (document.getElementById("annualSalIn").value)/12;
+                  //console.log(document.getElementById("annualSalIn").value);
+  //send user input to  addItem
+  addItem( firstName, lastName, idNum, jobTitle, annualSal);
+  console.log("This is the annual salary1:", annualSal);
+};
+// end clickFunction
