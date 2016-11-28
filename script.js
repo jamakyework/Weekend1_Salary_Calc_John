55f35389b89e10d56c4dd769c20c677ec2b32914
John Amakye <john.amakye@minneapolismn.gov>
2016-11-28 10:06:44 -0600

    submit to prime

--- script.js
+++ script.js
@@ -3,46 +3,81 @@ console.log("it works");
 // userInput array
 var userInput = [];
 
-//addItem
-var addItem = function( name1, name2, idNum1, jobTitle1, annualSal1 ){
-console.log( 'this is in addItem1:', name1, name2, idNum1, jobTitle1, annualSal1);
-// create newitem object
-  var newItem = {
-  name1:      name1,
-  name2:      name2,
-  idNum1:     idNum1,
-  jobTitle1:  jobTitle1,
-  annualSal1: annualSal1
-};
-   // end newItem object
-   // push newItem into the userInput array
-  userInput.push(newItem);
-   // end userInput push
-};
-//end var addItem
-
-console.log("This is in userInput: ", userInput);
-//check to see if push worked
+// console.log("This is in userInput: ", userInput);
+console.log("User userInput log1: ", userInput);
 
 // start clickFunction(acquire user input)
  var clickFunction = function() {
-  var firstName = document.getElementById("firstNameIn").value;
-                  //console.log(document.getElementById("firstNameIn").value);
-  var lastName =  document.getElementById("lastNameIn").value;
-                  //console.log(document.getElementById("lastNameIn").value);
-  var idNum =     document.getElementById("idNumIn").value;
-                  //console.log(document.getElementById("idNumIn").value);
-  var jobTitle =  document.getElementById("jobTitleIn").value;
-                  //console.log(document.getElementById("jobTitleIn").value);
-  var annualSal = (document.getElementById("annualSalIn").value);
-                  //console.log(document.getElementById("annualSalIn").value);
-  var monthlySal = annualSal/12;  //calculate monthly salary
-  addItem( firstName, lastName, idNum, jobTitle, annualSal); //send user input to  addItem
-  console.log("This is the monthlySal:", monthlySal);
-  // for (var i = 0; i < userInput.length; i++) {
-  //   console.log(clickFunction(userInput[i])); {
-  //     return clickFunction[i];
-  //   }
-  // }//aaargggghhhh infinite loop!!!!
+     var firstName = document.getElementById("firstName").value;
+                     console.log("First Name:", firstName);
+     var lastName =  document.getElementById("lastName").value;
+                     console.log("Last Name:", lastName);
+     var idNum =     document.getElementById("idNum").value;
+                     console.log("ID Number:", idNum);
+     var jobTitle =  document.getElementById("jobTitle").value;
+                     console.log("Job Title:", jobTitle);
+     var annualSal = (document.getElementById("annualSal").value);
+                     console.log("Annual Salary:", annualSal);
+     var monthlySal = annualSal/12;  //calculate monthly salary
+                      console.log("Monthly Salary:", monthlySal);
+     var newItem = [firstName, lastName, idNum, jobTitle, annualSal, monthlySal];
+      userInput.push(newItem); // push  into the userInput array
+      console.log("This is in userInput log2: ", userInput);//check to see if push worked
+        document.getElementById('fname').innerHTML+=(firstName);
+        document.getElementById('lname').innerHTML+=(lastName);
+        document.getElementById('idNum').innerHTML+=(idNum);
+        document.getElementById('jobTitle').innerHTML+=(jobTitle);
+        document.getElementById('annualSal').innerHTML+=(annualSal);
+        document.getElementById('totalMonthlySal').innerHTML+=(monthlySalCalc());
 };
-// end clickFunction
+// end clickFunctionfunction
+
+// // start reset form
+// function resetForm() {
+//   document.getElementById("addEmployee").reset();
+//   }
+// //end reset form
+
+//begin totalMonthlySalCalc2
+function monthlySalCalc() {
+    var startSal = 0;
+    for   (var i = 0; i < userInput.length; i++) {
+    startSal += userInput[i][5];
+    console.log("This is the total monthly cost of salary: ", startSal);
+    }
+    return startSal;
+  }
+  monthlySalCalc();
+//end totalMonthlySalCalc
+
+// //begin totalMonthlySalCalc1
+// var monthlySalCalc = function () {
+//     var startSal = 0;
+//     for   (var i = 0; i < userInput.length; i++) {
+//     startSal += userInput[i][5];
+//     console.log("This is the monthly salary: ", startSal);
+//     }
+//     return startSal;
+//   };
+// //end totalMonthlySalCalc
+
+//start create table function
+// function createTable(){
+// var row = table.insertRow(0);
+// cell1.innerHTML = "fname";
+// cell2.innerHTML = "lname";
+// cell1.innerHTML = "idNum";
+// cell2.innerHTML = "jobTitle";
+// cell1.innerHTML = "annualSal";
+// cell2.innerHTML = "monthlySal";
+// cell1.innerHTML = "fname";
+// cell2.innerHTML = "totalMonthlySal";
+//}
+//end create table function
+
+
+// var table = document.getElementById("outputSalary");
+// var row = table.insertRow(0);
+// var row = table.insertRow(0);
+// cell1.innerHTML = "NEW CELL1";
+// cell2.innerHTML = "NEW CELL2";
