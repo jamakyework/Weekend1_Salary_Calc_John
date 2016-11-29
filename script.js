84c482409da77bd3e9c077a5b7bc58f5b739e4b2
John Amakye <john.amakye@minneapolismn.gov>
2016-11-28 23:00:09 -0600

    cleaned up code

--- script.js
+++ script.js
@@ -19,14 +19,7 @@ var userInput = [];
                      console.log("Monthly Salary:", monthlySal);
     var newItem = [firstName, lastName, idNum, jobTitle, annualSal, monthlySal];
       userInput.push(newItem); //push  into the userInput array
-      console.log("This is in userInput log2: ", userInput);//check to see if push worked
-        // document.getElementById('outputTable').innerHTML+=(firstName);
-        // document.getElementById('outputTable').innerHTML+=(lastName);
-        // document.getElementById('outputTable').innerHTML+=(idNum);
-        // document.getElementById('outputTable').innerHTML+=(jobTitle);
-        // document.getElementById('outputTable').innerHTML+=(annualSal);
-        // document.getElementById('outputTable').innerHTML+=(monthlySal);
-        // document.getElementById('outputTable').innerHTML+=(monthlySalCalc());
+      console.log("This is in userInput: ", userInput);//check to see if push worked
         document.getElementById('outputTable').innerHTML+=("<tr><td>" + firstName +
         "</td><td>" + lastName + "</td><td>" + idNum + "</td><td>" + jobTitle +
         "</td><td>" + annualSal + "</td><td>" + monthlySal + "</td><td>" +
@@ -45,61 +38,8 @@ function monthlySalCalc() {
 }
 //end totalMonthlySalCalc2
 
+//begin clearForm function
 function clearForm() {
 document.getElementById("empInput").reset();
 }
-// //clear Form
-// var clearForm = function() {
-//   document.getElementById('firstname').innerHTML ="";
-//   document.getElementById('lastName').innerHTML ="";
-//   document.getElementById('idNum').innerHTML ="";
-//   document.getElementById('jobTitle').innerHTML ="";
-//   document.getElementById('annualSal').innerHTML ="";
-// };
-//
-// function clearForm() {
-//   document.getElementById('firstName').innerHTML ="";
-//   document.getElementById('lastName').innerHTML ="";
-//   document.getElementById('idNum').innerHTML ="";
-//   document.getElementById('jobTitle').innerHTML ="";
-//   document.getElementById('annualSal').innerHTML ="";
-// }
-
-
-
-// //begin totalMonthlySalCalc1
-// var monthlySalCalc = function () {
-//     var startSal = 0;
-//     for   (var i = 0; i < userInput.length; i++) {
-//     startSal += userInput[i][5];
-//     console.log("This is the monthly salary: ", startSal);
-//     }
-//     return startSal;
-//   };
-// //end totalMonthlySalCalc1
-
-//start create table function
-// function createTable(){
-// var row = table.insertRow(0);
-// cell1.innerHTML = "fname";
-// cell2.innerHTML = "lname";
-// cell1.innerHTML = "idNum";
-// cell2.innerHTML = "jobTitle";
-// cell1.innerHTML = "annualSal";
-// cell2.innerHTML = "monthlySal";
-// cell1.innerHTML = "fname";
-// cell2.innerHTML = "totalMonthlySal";
-//}
-//end create table function
-
-// var table = document.getElementById("outputSalary");
-// var row = table.insertRow(0);
-// var row = table.insertRow(0);
-// cell1.innerHTML = "NEW CELL1";
-// cell2.innerHTML = "NEW CELL2";
-
-// // start reset form
-// function resetForm() {
-//   document.getElementById("addEmployee").reset();
-//   }
-// //end reset form
+//end clearForm function
